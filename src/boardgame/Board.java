6693cdf615b339808507925017bcddef44f59c80
Th3PL <plucas2013@gmail.com>
2023-06-30 15:57:06 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,7 +7,7 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1 ) {
-			throw new BoardExeption("ERROR CREATING BOARD: there must be at least 1 row and 1 column");
+			throw new BoardException("ERROR CREATING BOARD: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -28,24 +28,37 @@ public class Board {
 	
 	public Piece piece(int row, int column) {
 		if(!positionExists(row, column)) {
-			throw new BoardExeption("Posisiton not on the board");
+			throw new BoardException("Posisiton not on the board");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardExeption("Posisiton not on the board");
+			throw new BoardException("Posisiton not on the board");
 		}
 		return pieces[position.getRow()][position.getColum()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
 		if(thereIsAPiece(position)) {
-			throw new BoardExeption("There is alredy a piece on position " + position);
+			throw new BoardException("There is alredy a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColum()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posisiton not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColum()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
@@ -58,7 +71,7 @@ public class Board {
 	
 	public boolean thereIsAPiece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardExeption("Posisiton not on the board");
+			throw new BoardException("Posisiton not on the board");
 		}
 		return piece(position) != null;
 	}
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessExeption("ERROR INSTANTIATING CHESSPOSITION. Valid values are from a1 to h8");
+			throw new ChessException("ERROR INSTANTIATING CHESSPOSITION. Valid values are from a1 to h8");
 		}
 		this.column = column;
 		this.row = row;
